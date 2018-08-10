3f04c9fb53375a214d9043e977ac0c2d6e461053
adielletuchman <adielle.tuchman@mjbha.org>
2018-08-10 18:44:57 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -1,4 +1,4 @@
-#1. Create a hash called `perfect_10_recipe` that has the following data:
+  #1. Create a hash called `perfect_10_recipe` that has the following data:
 #  - almond flour - 2 cups
 #  - gluten freewhole oats - 1 cup
 #  - kosher salt - 1/2 tea spoon
